5b57dd5d69caf2f41d433e5c5abdc8ff4221be9b
lvwanjie <lvwanjie@dangdang.com>
2020-01-21 10:29:24 +0800

    test commit2

---
+++ app/src/main/java/com/example/myapplication/anim/MoveActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.anim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.example.myapplication.R;
+
+public class MoveActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
